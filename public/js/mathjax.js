6e7bc7685bbe228f6e31993c164acf0c9389547d
sophiaboke <149358378+sophiaboke@users.noreply.github.com>
2025-01-31 22:07:08 +0800

    Delete public directory

--- public/js/mathjax.js
+++
@@ -1,8 +0,0 @@
-MathJax = {
-    tex: {
-        inlineMath: [['$', '$'], ['\\(', '\\)']],
-        displayMath: [['$$','$$'], ['\\[', '\\]'], ['\begin{equation}','\end{equation}'], ['\begin{align}','\end{align}']],
-        equationNumbers: { autoNumber: "AMS" },
-        extensions: ["AMSmath.js", "AMSsymbols.js"],
-    }
-};
